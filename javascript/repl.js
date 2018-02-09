0403fce2fa090521f09c895568b358bcbfd0fb6c
Yannick Francois <yaf@elsif.fr>
2018-02-09 18:42:03 +0100

    Reconnais 'select' et 'insert'

--- javascript/repl.js
+++ javascript/repl.js
@@ -1,14 +1,39 @@
+'use strict';
 
-process.stdout.write("> ");
+const availableCommands = ['select', 'insert']
 
-var stdin = process.openStdin();
+const valid = (command) => {
+	var splittedCommand = command.split(' ')
+	return availableCommands.indexOf(splittedCommand[0]) > -1
+}
+
+const exec = (command) => {
+	process.stdout.write(`Ceci est un ${command}, mais c'est pas encore implémenté\n`);
+}
+
+const printPrompt = () => {
+	process.stdout.write("db> ")
+}
+
+printPrompt()
+
+var stdin = process.openStdin()
 
 stdin.addListener("data", function(d) {
 	var command = d.toString().trim();
-	if(command == ".exit"){
-		process.exit(0);
-	} else {
-    process.stdout.write("Command non reconnue\n");
+
+	if(command[0] == '.') {
+		if(command == ".exit"){
+			process.exit(0)
+		}
 	}
-	process.stdout.write("> ");
+
+	if(valid(command)) {
+		exec(command)
+	} else {
+		process.stdout.write("Command non reconnue\n")
+	}
+
+	process.stdout.write("db> ")
+
 });
